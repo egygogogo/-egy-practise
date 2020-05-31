5751a9e66edb4299d5ab71c4431f040d6e8a2e27
egygogogo <657107260@qq.com>
2020-05-31 16:06:03 +0800

    完成放大镜代码

--- 京东放大镜练习/js/detail.js
+++ 京东放大镜练习/js/detail.js
@@ -57,3 +57,70 @@
 
 // })
 
+//在html和css加载完之后再执行js代码 
+window.addEventListener('load',function(){
+    //1 获取元素
+    var preview_img = document.querySelector('.preview_img');
+    var mask = document.querySelector('.mask');
+    var big = document.querySelector('.big'); 
+    
+    //2 鼠标经过preview_img 黄盒子和大盒子显示
+    preview_img.addEventListener('mouseover',function(){
+        mask.style.display = 'block';
+        big.style.display = 'block';
+    })
+
+    //3 鼠标离开的时候 黄盒子和大盒子隐藏
+    preview_img.addEventListener('mouseout',function(){
+        mask.style.display = 'none';
+        big.style.display = 'none';
+    })
+
+    
+    //4 黄色盒子跟随鼠标移动而移动
+    preview_img.addEventListener('mousemove',function(e){
+        var x = e.pageX - this.offsetLeft; // 距离浏览器左边多少，需要一层层审查父元素有无定位
+        var y = e.pageY - this.offsetTop;
+
+        // mask.style.left = moveX + 'px';
+        // mask.style.top = moveY + 'px';
+        // 5 单单这样肯定不ok，鼠标要在黄色盒子中央
+        // mask.style.left = x - mask.offsetWidth/2 + 'px';
+        // mask.style.top = y - mask.offsetHeight/2 + 'px';
+
+        // 6 以上写完发现盒子会飞到框框外面，限制一下黄色盒子的移动范围
+        // 思路是，如果盒子移动的距离超过了多少 就定一个0值和大盒子减去黄盒子的距离
+        // 判断盒子移动的距离
+        var maskX = x - mask.offsetWidth/2;
+        var maskY = y - mask.offsetHeight/2;
+
+        var maskMax = preview_img.offsetWidth - mask.offsetWidth;
+        
+        if (maskX <= 0){
+            maskX = 0;
+        }
+        else if(maskX >=  maskMax){
+            maskX = maskMax;
+        }
+        if (maskY <= 0){
+            maskY = 0;
+        }
+        else if(maskY >=  maskMax){
+            maskY = maskMax;
+        }
+
+        mask.style.left = maskX + 'px';
+        mask.style.top = maskY + 'px';
+
+        // 7 以上写完，盒子已经固定在方框里了。接下来开始写大图片跟随盒子移动而移动
+        // 8 需要得到大图片的参数
+        var bigIMg = preview_img.querySelector('.bigImg');
+        var bigMax = bigIMg.offsetWidth - big.offsetWidth;
+        // 9 为什么不动！为什么不动！！很暴躁！！
+        // 要记得给bigimg添加left和top啊，不给属性怎么赋值嘛。
+        bigIMg.style.left = - maskX * bigMax / maskMax + 'px';
+        bigIMg.style.top =  - maskY * bigMax / maskMax + 'px';
+
+    }) 
+
+})
